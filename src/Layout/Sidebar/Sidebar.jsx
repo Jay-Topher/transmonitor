d4577ebe8878b100023fd3199d426606cc1e24ee
Jones Ogolo <jay.topher11@gmail.com>
2020-01-16 23:53:49 +0100

    created a  sidebar container component

---
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.scss'
+import InvoiceButton from '../../components/InvoiceButton/InvoiceButton'
+
+const Sidebar = () => {
+  return (
+    <div className="Sidebar">
+      <InvoiceButton />
+    </div>
+  )
+}
+
+export default Sidebar
