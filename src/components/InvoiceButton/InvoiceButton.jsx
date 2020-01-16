061ed36e6dc35634e361a1b5fb079bb4bbc103f0
Jones Ogolo <jay.topher11@gmail.com>
2020-01-16 23:10:49 +0100

    created and styled the invoice button component

---
+++ src/components/InvoiceButton/InvoiceButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './InvoiceButton.scss'
+
+const InvoiceButton = () => {
+  return (
+    <button className="InvoiceButton">
+      generate invoice
+    </button>
+  )
+}
+
+export default InvoiceButton
