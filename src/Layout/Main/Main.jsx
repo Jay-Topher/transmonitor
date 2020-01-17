436431835b7d30efec800ce3247afb0a40aa4433
Jones Ogolo <jay.topher11@gmail.com>
2020-01-17 01:48:19 +0100

    mounted the transaction container

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import './Main.scss'
+import React from "react";
+import "./Main.scss";
+import TransactionContainer from "../../components/TransactionContainer/TransactionContainer";
 
 const Main = () => {
   return (
     <div className="Main">
-      
+      <div className="Main_child bordered">
+        <TransactionContainer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
