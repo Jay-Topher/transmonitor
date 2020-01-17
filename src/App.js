8f0aa63d264156409459887415a5a9134c8e34cd
Jones Ogolo <jay.topher11@gmail.com>
2020-01-17 04:14:21 +0100

    renamed class name and style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
+      <div className="containers">
         <Sidebar />
         <Main />
       </div>
